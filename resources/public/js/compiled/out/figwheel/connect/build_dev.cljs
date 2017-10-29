e1877a84199449883cb9d6286877d16631424aa8
Linyxus <linyxus@outlook.com>
2017-10-29 14:41:23 +0800

    Useless files.:

--- resources/public/js/compiled/out/figwheel/connect/build_dev.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect.build-dev (:require [space.core] [figwheel.client] [figwheel.client.utils]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/space.core.on-js-reload (apply js/space.core.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'space.core/on-js-reload' is missing"))), :open-urls ["http://localhost:3449/index.html"], :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
