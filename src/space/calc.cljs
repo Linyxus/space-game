ac63c2d9d7055c8df9e4a58daefffa8251617a94
linyxus <linyxus@outlook.com>
2017-11-11 20:31:09 +0800

    Gravity calculation is compeleted.

--- src/space/calc.cljs
+++ src/space/calc.cljs
@@ -83,4 +83,51 @@
       (- angle)
       angle)))
 
-(rotation-by-vector [0.1 0.1])
+(defn vec-by-length
+  "Returns a new vector of given length having the same direction with given."
+  [v len]
+  (vec* len (vecdiv v (vecmode v))))
+
+;; Compute data format:
+;; {:objects [{:x 0.1, :y 0.1, :v 1.0, :a 0.5}],
+;;  :forcefields [(fn [x y] [1.0 1.0])]}
+
+(defn gravity-forcefield
+  "Returns a function that can be used as a forcefield of gravity."
+  [x y m]
+  (fn [ox oy]
+    (let [d (distance [x y] [ox oy])
+          f (/ (* m 0.001) (square d))
+          a (vec-by-length (vec+ [x y] (vec- [ox oy])) f)]
+      a)))
+
+(defn next-state-each
+  "Returns the next state for each object."
+  [craft forcefields dt]
+  (let [{:keys [x y v a]} craft
+        force-a (map #(% x y) forcefields)
+        sum-a (apply vec+ force-a)
+        sum-a (vec+ sum-a a)
+        dv (vec* dt sum-a)
+        ddv (vecdiv dv 2)
+        v-bar (vec+ v ddv)
+        dx (vec* dt v-bar)
+        [xx yy] (vec+ [x y] dx)
+        vv (vec+ v dv)]
+    (assoc craft :x xx :y yy :v vv)))
+
+;; (def test-state
+;;   {:objects [{:x 0.13, :y 0.25, :v [0 0], :a [0.3 2.1]}
+;;              {:x 0.12, :y 0.25, :v [1 0], :a [0.3 2.1]}
+;;              {:x 0.1, :y 0.32, :v [0 0], :a [0.3 2.1]}]
+;;    :forcefields [(gravity-forcefield 0.1 0.2 5.0)
+;;                  (gravity-forcefield 0.3 0.1 2.3)]})
+
+(defn next-state
+  "Returns the next state by given data."
+  [data dt]
+  (let [{:keys [objects forcefields]} data]
+    (map next-state-each objects
+         (repeat forcefields)
+         (repeat dt))))
+
